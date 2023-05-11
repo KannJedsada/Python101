dc8ec0c9d5afc9bfdfb99c99fe87a2bee11926ab
KannJedsada <133192794+KannJedsada@users.noreply.github.com>
2023-05-11 15:01:37 +0700

    แก้ไขHello world กับเพิ่ม basicpackage

---
+++ ep02module/info.py
@@ -0,0 +1,2 @@
+name = 'Jedsada'
+lastname = 'Chananee'
--- hello world.py
+++ hello world.py
@@ -1,3 +1,8 @@
 print('Hello World')
 print('Hello World')
-print('Hello World')
+print('Hello Uncle')
+
+print('Hello Github')
+
+friend = ['loong','catty','Pa Lek']
+print(friend[0])
